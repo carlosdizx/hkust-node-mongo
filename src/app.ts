28d96fe9eb040b85561ff37e10b719adf0496f9b
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-22 23:53:24 -0500

    :sparkles: configure app for connect to Mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("Starting app");
+import { MongoClient, Db } from "mongodb";
+import assert from "assert";
+
+const uri = "mongodb://localhost:27017/";
+const dbName = "confusion-server";
+
+const connectToMongo = async () => {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log("Conexión exitosa a MongoDB");
+
+    const database = client.db(dbName);
+    const collection = database.collection("dishes");
+    await collection.insertOne({ name: "Uthapizza", description: "test" });
+  } catch (error) {
+    console.error("Error al conectarse a MongoDB:", error);
+    throw error;
+  }
+};
+(async () => {
+  await connectToMongo();
+})();
